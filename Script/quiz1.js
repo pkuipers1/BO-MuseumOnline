8594e2461a54c5a46854f2e7df64a874b3184f82
Patricia Kuipers <pkpatriciakuipers@gmail.com>
2021-01-15 05:48:51 +0100

    Stukje waarom LAM zo leuk is toegevoegd, quiz start

---
+++ Script/quiz1.js
@@ -0,0 +1,75 @@
+const quiz1 =
+{
+    "quizID": "Museum 1",
+    "quizMetaData":
+    {
+        "title": "Welk kunstwerk ben jij?"
+    },
+    "quizContent":
+		[
+			{
+				"question": "Hoe is jouw zicht op het leven?",
+				"answers": [{
+						"answer": "Optimistisch",
+						"feedback": true
+					},
+					{
+						"answer": "Realistisch",
+						"feedback": true
+					},
+					{
+						"answer": "Pessimistisch",
+						"feedback": false
+					}
+				]
+			},
+			{
+				"question": "Een van je familieleden vraagt om hulp. Wat doe je?",
+				"answers": [{
+						"answer": "Je legt je werk opzij en helpt",
+						"feedback": false
+					},
+					{
+						"answer": " Je legt uit dat je het nu even druk hebt, maar over een kwartiertje wel wilt helpen",
+						"feedback": true
+					},
+					{
+						"answer": " Je weigert te helpen, je hebt het zelf al druk genoeg",
+						"feedback": false
+					}
+				]
+			},
+			{
+				"question": "Je moet voor school een tekening maken. Wat voor tekening maak je?",
+				"answers": [{
+						"answer": "Een fantasie gevecht tussen een reusachtige draak en een robot van de toekomst.",
+						"feedback": false
+					},
+					{
+						"answer": "Een mooi rustgevend stilleven",
+						"feedback": false
+					},
+					{
+						"answer": "Je maakt een screenshot van je favoriete game en tekent het na.",
+						"feedback": false
+					}
+				]
+			},
+			{
+				"question": "Er is een deadline vanavond, maar je bent nog niet klaar. Wat ga je doen?",
+				"answers": [{
+						"answer": "Direct actie ondernemen. Je gaat proberen het nog op tijd in te leveren.",
+						"feedback": false
+					},
+					{
+						"answer": "Je begint eraan, maar eigenlijk weet je al dat je het niet af gaat redden. Je legt het uit aan de docent en levert het na de deadline in.",
+						"feedback": false
+					},
+					{
+						"answer": "Je maakt de opdracht niet. Je red het toch niet voor vanavond.",
+						"feedback": false
+					}
+				]
+			}
+		]
+	}
